c19e1d12391829a521350ae91b558fa68c821e0d
Jason Chew <70564653+BioxMech@users.noreply.github.com>
2022-12-05 22:48:58 +0800

    Updated about page and enabled husky

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -36,7 +36,7 @@ const StyledText = styled.div`
       font-size: var(--fz-xs);
 
       &:before {
-        content: '▹';
+        content: "▹";
         position: absolute;
         left: 0;
         color: var(--green);
@@ -89,7 +89,7 @@ const StyledPic = styled.div`
 
     &:before,
     &:after {
-      content: '';
+      content: "";
       display: block;
       position: absolute;
       width: 100%;
@@ -126,7 +126,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'NestJS',
+    'Node.js',
+    'Python',
+    'MongoDB',
+    'AWS',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +145,26 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Jason and I enjoy creating things that live on
+              the internet. My interest in full-stack development, essentially
+              the Frontend, Backend and Infrastructure development. I started
+              back in 2019 when I decided to try building websites using HTML
+              &amp; CSS. I practically built a e-commerce website — turns out
+              hacking together a custom payment page taught me a lot about HTML
+              & CSS!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://www.tech.gov.sg/">a public sector agency</a>,{' '}
+              <a href="https://www.viki.com/">
+                a streaming platform corporation
+              </a>
+              ,{' '}
+              <a href="https://www.ocbc.com/group/gateway.page/  ">
+                a huge financial institution
+              </a>
+              , and <a href="https://wedbio.com/">a startup</a>.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
